8b5c37aafff973517151e0232c99ddba2abd811e
SethG1262 <151973945+SethG1262@users.noreply.github.com>
2024-09-21 16:33:47 -0600

    Week-2
    Week-2 tasks assignment

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Seth Garciano</h2>
+      <p>
+        GitHub: <Link href="https://github.com/SethG1262/cprg306-assignments">https://github.com/SethG1262/cprg306-assignments</Link>
+      </p>
+    </div>
+  );
+}
